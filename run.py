52fb1fd1ad05a993914ad5e7321bef0e6edeaecd
vfg-95 <sam.ridding@hotmail.co.uk>
2022-02-12 17:10:43 +0000

    Remove unecessary display functions

--- run.py
+++ run.py
@@ -40,8 +40,6 @@ def check_letter(cur_game, guess):
     elif guess not in cur_game.cur_word:
         cur_game.remaining_guesses -= 1
         cur_game.guessed_letters.append(guess)
-        #clear_screen(cur_game)
-        #update_display(cur_game)
     elif guess in cur_game.cur_word:
         cur_game.guessed_letters.append(guess)
         word_ltrs = list(cur_game.hidden_word)
@@ -49,8 +47,6 @@ def check_letter(cur_game, guess):
         for index in indices:
             word_ltrs[index] = guess
         cur_game.hidden_word = "".join(word_ltrs)
-        #clear_screen(cur_game)
-        #update_display(cur_game)
 
 
 def already_guessed(cur_game, guess):
